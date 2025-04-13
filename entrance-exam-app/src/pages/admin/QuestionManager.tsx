fee139464f614593b85ca2fa65f47c149633492f
Jhuleebaba <jhulee1@gmail.com>
2025-04-14 00:08:19 +0100

    Update QuestionManager.tsx

--- entrance-exam-app/src/pages/admin/QuestionManager.tsx
+++ entrance-exam-app/src/pages/admin/QuestionManager.tsx
@@ -24,6 +24,7 @@ import {
 } from '@mui/material';
 import { Delete as DeleteIcon, Edit as EditIcon } from '@mui/icons-material';
 import axios from 'axios';
+import { SelectChangeEvent } from '@mui/material';
 
 interface Question {
   _id: string;
